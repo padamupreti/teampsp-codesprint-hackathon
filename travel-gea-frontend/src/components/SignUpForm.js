a8b78fcfbc5220ffa771df6f1850900c8d145bf0
padamupreti <59172926+padamupreti@users.noreply.github.com>
2025-07-01 10:48:26 +0545

    Add initial login/signup

---
+++ travel-gea-frontend/src/components/SignUpForm.js
@@ -0,0 +1,59 @@
+function SignUpForm({ handleSubmit }) {
+    return (
+        <form method="POST" className="mt-2" onSubmit={handleSubmit}>
+            <div className="mb-5">
+                <label
+                    htmlFor="signup-email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                    Email
+                </label>
+                <input
+                    type="email"
+                    name="email"
+                    id="signup-email"
+                    required
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+            </div>
+            <div className="mb-5">
+                <label
+                    htmlFor="signup-username"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                    Username
+                </label>
+                <input
+                    type="text"
+                    name="username"
+                    id="signup-username"
+                    required
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+            </div>
+            <div className="mb-5">
+                <label
+                    htmlFor="signup-password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                    Password
+                </label>
+                <input
+                    type="password"
+                    name="password"
+                    id="signup-password"
+                    required
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+            </div>
+            <button
+                type="submit"
+                className="cursor-pointer text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+                Sign Up
+            </button>
+        </form>
+    )
+}
+
+export default SignUpForm
