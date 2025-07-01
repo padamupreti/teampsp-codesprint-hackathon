bd64be838649dcbb17a7a398d732acc63b5d7bf6
padamupreti <59172926+padamupreti@users.noreply.github.com>
2025-07-01 12:16:26 +0545

    Show user state in home

--- travel-gea-frontend/src/pages/index.js
+++ travel-gea-frontend/src/pages/index.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react"
+
 function Home() {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const jsonData = localStorage.getItem("userData")
+        if (jsonData) setUser(JSON.parse(jsonData))
+    }, [])
+
     return (
         <div className="grid place-items-center h-full w-full">
-            <span className="text-2xl font-bold">Hello World</span>
+            {user && (
+                <span className="text-2xl font-bold">
+                    Hello {user.detail.userName}
+                </span>
+            )}
         </div>
     )
 }
